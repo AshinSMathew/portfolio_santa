7e5ebcfcff231de8c463302176f48055aaa35331
Ashin Sabu Mathew <ashinmathew1010@gmail.com>
2024-12-24 17:53:19 +0300

    semicircle adjusted

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-// Function to hide the navbar on mobile devices
 function toggleNavbar() {
   const navbar = document.querySelector('.navbar');
   
-  if (window.innerWidth <= 768) { // Check if the window width is 768px or smaller (mobile view)
-      navbar.style.display = 'none'; // Hide navbar on mobile
+  if (window.innerWidth <= 768) {
+      navbar.style.display = 'none';
   } else {
-      navbar.style.display = 'flex'; // Show navbar for larger screens
+      navbar.style.display = 'flex';
   }
 }
-
-// Event listener to check window resize
 window.addEventListener('resize', toggleNavbar);
-
-// Call the function initially to set the navbar state on page load
 toggleNavbar();
